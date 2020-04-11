9a91b40999c58b1e3fa97bd2a339b6a42866fca2
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-04-11 17:00:51 -0300

    :tada: finish challenge

---
+++ README.md
@@ -0,0 +1,36 @@
+<p align="center">
+    <img alt="GoStack" src="https://ap.imagensbrasil.org/images/2020/04/09/banner-bootcamp-gostack-11.png" />
+</p>
+
+## :rocket: Challenge 03: ReactJS Concepts
+
+- [About Challenge](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs)
+
+Challenge completed on 11/04/2020.
+
+
+## ⚙️ Getting Started
+
+```Bash
+# Install dependencies
+yarn install
+
+# Run application
+yarn start
+
+# Run tests
+yarn test
+```
+> Access the URL in your browser http://localhost:3000
+
+## Demo
+
+<img alt="Demo" src="https://i1.lensdump.com/i/jCEjMP.gif" height="300" />
+
+## :man_astronaut: Author
+
+**Guilherme Bezerra** ©️
+
+[![github](http://ap.imagensbrasil.org/images/2018/12/10/github-logo-1.png) ](http://www.github.com/gbdsantos)
+[![gitlab](http://ap.imagensbrasil.org/images/2018/12/10/gitlab-32.png)](https://gitlab.com/gbdsantos1)
+[![linkedin](http://ap.imagensbrasil.org/images/2018/12/10/linkedin-1.png)](https://www.linkedin.com/in/gbdsantos/)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  /**TODO: Listar os repositórios da sua API: 
+   * Deve ser capaz de criar uma lista com o campo title de todos os repositórios que estão cadastrados na sua API.
+   */
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  /** TODO: Adicionar um repositório a sua API: 
+   * Deve ser capaz de adicionar um novo item na sua API através de um botão 
+   * com o texto Adicionar e, após a criação, deve ser capaz de exibir o nome
+   * dele após o cadastro. */
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo repositório ${Date.now()}`,
+      owner: "Guilherme Bezerra"
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
+  /**TODO: Remover um repositório da sua API  : 
+  * Para cada item da sua lista, deve possuir um botão com o texto Remover que,
+  * ao clicar, irá chamar uma função para remover esse item da lista do seu frontend e da sua API.
+  */
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const repository = repositories.filter(
+      repository => repository.id !== id
+    );
+    setRepositories(repository);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
